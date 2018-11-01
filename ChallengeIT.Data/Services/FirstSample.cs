72873b681145e8d2638fd8c409a0ca07d6a0e323
INFOTRACK\rocco.smit <rocco.smit@infotrack.com.au>
2018-11-01 11:52:26 +1100

    Added a basic example for database to serviec layer interfaces

---
+++ ChallengeIT.Data/Services/FirstSample.cs
@@ -0,0 +1,19 @@
+﻿using ChallengeIT.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeIT.Data.Services
+{
+    internal class FirstSample : IFirstSample
+    {
+        public int SaveData(string value1, string value2)
+        {
+            return 1;
+
+            // Your database code goes here
+        }
+    }
+}
